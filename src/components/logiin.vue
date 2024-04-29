54ede624fb9565d3f1050828a088432f21c32e07
hln <128910620+1920604577@users.noreply.github.com>
2024-04-29 21:28:40 +0800

    Add files via upload

---
+++ src/components/logiin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login-wrap">
+    <div class="ms-title">music 后台管理系统</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.login-wrap{
+    position: relative;
+    background: url("../assets/img/background.jpg");
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+.ms-title{
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    margin-top: -230px;
+    text-align: center;
+    
+}
+</style>
